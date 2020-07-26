37c0dcd024c299fba33e5e566071473b57686607
Carlos <c.hdezbueno@gmail.com>
2020-07-26 12:29:01 +0200

    Full funcionando

--- flaskapp.py
+++ flaskapp.py
@@ -55,38 +55,62 @@ class CheckStatus(Resource):
 
 threadLock = threading.Lock()
 comandos = {"posix": {'puertoSerie': '/dev/ttyUSB0'},
-                    "nt": {'puertoSerie': 'COM3'}}
+            "nt": {'puertoSerie': 'COM3'}}
 
 
 class ArduinoConnection(threading.Thread):
 
+    __instance = None
+    arduino = None
+
+    @staticmethod
+    def get_instance():
+        if ArduinoConnection.__instance == None:
+            
+            threadLock.acquire() #Asi se evita que dos hilos accedan simultameamente
+            
+            """Se comprueba de nuevo si instance es nula, ya que si entran dos hilos
+            A la vez, uno de ellos podria haberla inicializado ya antes del actual"""
+
+            if ArduinoConnection.__instance == None:
+                ArduinoConnection()
+           
+            threadLock.release() #Se notifica que se acabo el bloqueo
+        
+        return ArduinoConnection.__instance
+
     def __init__(self):
         threading.Thread.__init__(self)
+        
+        #Lanza una excepcion si instance no es None
+        if ArduinoConnection.__instance != None:
+            raise Exception("Es una clase singleton") 
+        else:
+            self.arduino = serial.Serial(
+                comandos[os.name]["puertoSerie"], 9600, timeout=5)
+            self.arduino.readline()  # Vacia el buffer completamente
+
+            ArduinoConnection.__instance = self
 
     def getDataArduino(self):
         userID = gma()
 
         threadLock.acquire()
         try:
-            arduino = serial.Serial(
-                comandos[os.name]["puertoSerie"], 9600, timeout=5)
 
-            arduino.flushInput()
-            arduino.flushOutput()
+            self.arduino.flushInput()
+            self.arduino.flushOutput()
 
         except serial.SerialException:
             return None, None
-        with arduino:
+        
+        self.arduino.write(b'enviame')
 
-            arduino.read()  # Limpia el buffer de entrada
+        self.arduino.flush()
 
-            arduino.write(b'enviame')
+        self.arduino.readline()  # Elimina la palabra enviada anteriormente
 
-            arduino.flush()
-
-            arduino.readline()  # Elimina la palabra enviada anteriormente
-
-            datos = arduino.readline()  # Recibe los datos reales
+        datos = self.arduino.readline()  # Recibe los datos reales
 
         threadLock.release()
         return userID, datos
@@ -94,34 +118,25 @@ class ArduinoConnection(threading.Thread):
     def sendDataArduino(self, orden):
 
         threadLock.acquire()
+        
+        self.arduino.write(orden.encode())
+        self.arduino.flush()
 
-        arduino = serial.Serial(
-            comandos[os.name]["puertoSerie"], 9600, timeout=1)
+        # Elimina los datos del buffer y la palabra enviada anteriormente
+        self.arduino.readline()
 
-        with arduino:
-            arduino.readline()  # Vacia el buffer del puerto serie para enviar datos despues
-            arduino.write(orden.encode())
-            arduino.flush()
-
-            # Elimina los datos del buffer y la palabra enviada anteriormente
-            arduino.readline()
-            arduino.readline()
-
-            confirmacion = arduino.readline().decode(
-                "utf-8").replace('\r\n', '')  # Recibe la confirmacion de recepcion
+        confirmacion = self.arduino.readline().decode(
+            "utf-8").replace('\r\n', '')  # Recibe la confirmacion de recepcion
 
         threadLock.release()
-        
+
         if confirmacion == "OK":
             return "OK-Orden realizada"
         elif confirmacion == "No":
             return "No existe la orden: <" + orden + ">"
         else:
             return "Error desconocido"
-        
 
-
-    
 
 # define endpoint args parser
 argument_parser = reqparse.RequestParser()
@@ -139,27 +154,32 @@ class GrabDataArduino(Resource):
     @api.expect(argument_parser)
     def post(self):
         """ask the arduino for data and insert data in db"""
+        
+        codRespuesta = 200
 
-        arduinoDao = ArduinoConnection()
+        arduinoDao = ArduinoConnection.get_instance()
 
         userID, datos = arduinoDao.getDataArduino()
-        
+
         if datos and self._validateJSON(datos):
             datos = json.loads(datos.decode("utf-8"))
-            
-            temperatura, humedad, luz, movimiento = datos["temperatura"], datos["humedad"], datos["luz"], datos["movimiento"]
-            status = self._insert_db(temperatura, humedad, luz, movimiento, userID)
+
+            temperatura, humedad, luz, movimiento = datos["temperatura"], datos[
+                "humedad"], datos["luz"], datos["movimiento"]
+            status = self._insert_db(
+                temperatura, humedad, luz, movimiento, userID)
 
         elif datos and datos.decode("utf-8") == "":
             status = "No se realizo medicion"
+            codRespuesta = 502
         else:
             status = "No esta OK mi pana"
-        
+            codRespuesta = 500
+
         return {
             "status": status,
             "data": datos or {}
-        }
-
+        }, codRespuesta
 
     @classmethod
     def _insert_db(cls, temperatura, humedad, luz, movimiento, userID):
@@ -222,7 +242,7 @@ class SQLGet(Resource):
 
     @classmethod
     def _selectKeys(cls, data, keys):
- 
+
         return [{key: datos.pop(key) if key != "timestamp" else datos.pop(key).strftime("%m/%d/%Y, %H:%M:%S") for key in keys} for datos in data]
 
 
@@ -245,7 +265,15 @@ class SendOrder(Resource):
         """write data in arduino serial port"""
         orden = self._get_args()
 
-        arduinoDao = ArduinoConnection()
+        if self.esNumero(orden):
+            intOrden = int(orden)
+            if intOrden <= 1 or intOrden > 40:
+
+                return {
+                    "status": "Error, solo dispones de los pines del 2 al 40"
+                }, 400
+
+        arduinoDao = ArduinoConnection.get_instance()
 
         status = arduinoDao.sendDataArduino(orden)
 
@@ -258,3 +286,11 @@ class SendOrder(Resource):
         orden = args['orden']
 
         return orden
+
+    @classmethod
+    def esNumero(cls, cadena):
+        try:
+            int(cadena)
+            return True
+        except ValueError:
+            return False
