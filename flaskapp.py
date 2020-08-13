ea0b3e36b0d12febc56b8012b373a7b9c9eeaa29
Carlos <c.hdezbueno@gmail.com>
2020-08-13 16:43:55 +0200

    apipath

--- flaskapp.py
+++ flaskapp.py
@@ -50,6 +50,56 @@ class CheckStatus(Resource):
     def get(self):
         return {"status": "OK"}
 
+#Nuevo
+
+argument_parserGetApiPath = reqparse.RequestParser()
+argument_parserGetApiPath.add_argument(
+    'userID', required=False, type=str, location='json', help='missing userID parameter')
+
+@api.route("/getApiPath")
+@api.doc(
+    params={
+        'userID' : 'Identificador del usuario'
+    }
+)
+class getApiPath(Resource):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.parser = argument_parserGetApiPath
+
+
+    @api.expect(argument_parserGetApiPath)
+    def get(self):
+        """get data from db"""
+        
+        #userID = self._get_args()
+        userID = request.args['userID']
+
+        resultado = mongoDB.select("usuarios", userID)
+
+        apiPath = ""
+        if resultado:
+            resultado = resultado.next()
+            apiPath = 'http://' + resultado['ipAddress'] + ':' + str(puertoApp)  + '/v1'
+        
+        
+
+        return {
+            "status": "OK",
+            "apiPath": apiPath
+        }
+
+    def _get_args(self):
+        args = self.parser.parse_args()
+
+        userID = args['userID']
+
+        return userID
+
+
+#Fin nuevo
+
+
 # define endpoint args parser
 argument_parser = reqparse.RequestParser()
 
